091fe8f4234efa54daadf330c15f4255f544bf4a
Evandra Raditya <evandraraditya049@gmail.com>
2023-09-17 23:06:03 +0700

    feat: authentication | create validation for login and register

--- app/Services/Auth/AuthenticationService.ts
+++ app/Services/Auth/AuthenticationService.ts
@@ -3,10 +3,12 @@ import type { RequestContract } from '@ioc:Adonis/Core/Request'
 import { AuthContract } from '@ioc:Adonis/Addons/Auth'
 import User from '../../Models/User'
 import Hash from '@ioc:Adonis/Core/Hash'
+import LoginRequestValidator from 'App/Validators/Auth/LoginRequestValidator'
+import RegisterRequestValidator from 'App/Validators/Auth/RegisterRequestValidator'
 
 export default class AuthenticationService {
   public async register(request: RequestContract) {
-    const { username, password } = request.body()
+    const { username, password } = await request.validate(RegisterRequestValidator)
     if (await User.findBy('username', username)) {
       throw new Exception('Username Already Taken', 409, 'E_DUPLICATE_ENTRY')
     }
@@ -18,7 +20,7 @@ export default class AuthenticationService {
   }
 
   public async login(auth: AuthContract, request: RequestContract) {
-    const { username, password } = request.body()
+    const { username, password } = await request.validate(LoginRequestValidator)
 
     const user = await User.findBy('username', username)
 
